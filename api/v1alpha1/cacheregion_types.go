112366a9d2c663624f04ea2bc647763e2752e00f
Ryan Emerson <remerson@redhat.com>
2022-05-25 15:47:44 +0100

    CacheRegion scaffolding

---
+++ api/v1alpha1/cacheregion_types.go
@@ -0,0 +1,48 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// CacheRegionSpec defines the desired state of CacheRegion
+type CacheRegionSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of CacheRegion. Edit cacheregion_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// CacheRegionStatus defines the observed state of CacheRegion
+type CacheRegionStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// CacheRegion is the Schema for the cacheregions API
+type CacheRegion struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   CacheRegionSpec   `json:"spec,omitempty"`
+	Status CacheRegionStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// CacheRegionList contains a list of CacheRegion
+type CacheRegionList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []CacheRegion `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&CacheRegion{}, &CacheRegionList{})
+}
