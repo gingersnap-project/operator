304a60f0274e4cff8b761a40bf95aa4d7e7f500c
Ryan Emerson <remerson@redhat.com>
2022-10-04 10:06:39 +0100

    Makefile is now the authoritative source of the Operator version
    Utilise 0.0.0 placeholder in Operator csv to avoid needing to update
    value on each release.
    
    ./hack scripts replaced with Makefile entries where appropriate
    
    The default IMAGE_TAG_BASE is now localhost:5000/operator, so any
    actions that require a push to a remote repository need to explicitly
    set IMAGE_TAG_BASE.

--- hack/common.sh
+++ hack/common.sh
@@ -4,17 +4,7 @@ set -e
 _script="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
 export PATH="$_script/../bin":$PATH
-export TESTING_NAMESPACE=${TESTING_NAMESPACE-namespace-for-testing}
-export IMG_REGISTRY=${IMG_REGISTRY-"localhost:5000"}
-export IMG_VERSION=${IMG_VERSION-"latest"}
 
-export VERSION=${VERSION-"0.0.1"}
-export DEFAULT_CHANNEL=alpha
 export CATALOG_SOURCE_NAME=${CATALOG_SOURCE_NAME-test-catalog}
 export CATALOG_SOURCE_NAMESPACE=${CATALOG_SOURCE_NAMESPACE-olm}
-
-export IMAGE_TAG_BASE=${IMG_REGISTRY}/operator
-export BUNDLE_IMG_TAG=${BUNDLE_IMG_TAG-"v${VERSION}"}
-export BUNDLE_IMG=${IMAGE_TAG_BASE}-bundle:${BUNDLE_IMG_TAG}
-export CATALOG_IMG=${IMAGE_TAG_BASE}-catalog
-export IMG=${IMAGE_TAG_BASE}:${IMG_VERSION}
+export SUBSCRIPTION_NAMESPACE=${SUBSCRIPTION_NAMESPACE-operators}
