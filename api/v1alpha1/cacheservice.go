220d831e888897dca0b5313a594a00afd7a76391
Ryan Emerson <remerson@redhat.com>
2022-10-25 15:14:38 +0100

    Remove Pod Sidecar proxy injector logic

--- api/v1alpha1/cacheservice.go
+++ api/v1alpha1/cacheservice.go
@@ -5,19 +5,6 @@ const (
 	LabelCacheNamespace = LabelCache + "-namespace"
 )
 
-func CacheServiceLabelsExist(m map[string]string) bool {
-	_, cacheExists := m[LabelCache]
-	_, namespaceExists := m[LabelCacheNamespace]
-	return cacheExists && namespaceExists
-}
-
-func CacheServiceFromLabels(m map[string]string) CacheService {
-	return CacheService{
-		Namespace: m[LabelCacheNamespace],
-		Name:      m[LabelCache],
-	}
-}
-
 func (s CacheService) ApplyLabels(m map[string]string) {
 	m[LabelCache] = s.Name
 	m[LabelCacheNamespace] = s.Namespace
