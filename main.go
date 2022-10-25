a24c119e95e1697dff9662b97ac443a1cd9941b5
Ryan Emerson <remerson@redhat.com>
2022-10-25 15:14:38 +0100

    Remove Pod Sidecar proxy injector logic

--- main.go
+++ main.go
@@ -22,18 +22,15 @@ import (
 	"strings"
 
 	"github.com/gingersnap-project/operator/pkg/kubernetes"
-	"github.com/gingersnap-project/operator/pkg/reconcile/sidecar"
 	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 	"sigs.k8s.io/controller-runtime/pkg/cache"
-
-	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
-	// to ensure that exec-entrypoint and run can make use of them.
-	_ "k8s.io/client-go/plugin/pkg/client/auth"
-	"sigs.k8s.io/controller-runtime/pkg/webhook"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
+	// to ensure that exec-entrypoint and run can make use of them.
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
@@ -114,11 +111,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err = (&controllers.PodReconciler{Reconciler: reconciler}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "Pod")
-		os.Exit(1)
-	}
-
 	if err = (&gingersnapv1alpha1.Cache{}).SetupWebhookWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create webhook", "webhook", "Cache")
 		os.Exit(1)
@@ -128,10 +120,6 @@ func main() {
 		os.Exit(1)
 	}
 	//+kubebuilder:scaffold:builder
-
-	// Register the Sidecar Proxy injector webhook
-	hookServer := mgr.GetWebhookServer()
-	hookServer.Register("/mutate-v1-pod", &webhook.Admission{Handler: &sidecar.ProxyInjector{Client: mgr.GetClient()}})
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
 		setupLog.Error(err, "unable to set up health check")
--- pkg/reconcile/region/configmap.go
+++ pkg/reconcile/region/configmap.go
@@ -4,12 +4,9 @@ import (
 	"fmt"
 
 	"github.com/gingersnap-project/operator/api/v1alpha1"
-	"github.com/gingersnap-project/operator/pkg/kubernetes/client"
 	"github.com/gingersnap-project/operator/pkg/reconcile"
 	"gopkg.in/yaml.v2"
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func configMapLabels(cacheService v1alpha1.CacheService) map[string]string {
@@ -47,82 +44,6 @@ func UpdateConfigMaps(r *v1alpha1.CacheRegion, ctx reconcile.Context) {
 			return
 		}
 	}
-}
-
-func CreateConfigMap(namespace string, service v1alpha1.CacheService, k8sClient client.Client) (*corev1.ConfigMap, error) {
-	k8sClient = k8sClient.WithNamespace(namespace)
-	labels := configMapLabels(service)
-	configMapData, err := ConfigMapData(service, k8sClient)
-	if err != nil {
-		return nil, fmt.Errorf("unable to initialize CacheRegion ConfigMap data: %w", err)
-	}
-
-	// Create ConfigMap using corev1 type so that we make use of GenerateName
-	configMap := &corev1.ConfigMap{
-		ObjectMeta: metav1.ObjectMeta{
-			Namespace:    namespace,
-			GenerateName: "gingersnap-regions-",
-			Labels:       labels,
-		},
-		BinaryData: configMapData,
-	}
-
-	if err := k8sClient.Create(configMap); err != nil && !errors.IsAlreadyExists(err) {
-		return nil, fmt.Errorf("unable to create CacheRegion ConfigMap: %w", err)
-	}
-
-	// We must loop until the created ConfigMap can be retrieved so that the generated ConfigMap name can be used by
-	// the sidecar container.
-	// Any error other than NotFound is treated as a failure.
-	// If for some unforeseen reason the ConfigMap is never found, the Webhook will timeout and the context will be cancelled.
-	for {
-		if configMap, err = FindExistingConfigMap(namespace, service, k8sClient); err != nil {
-			return nil, err
-		} else if configMap != nil {
-			return configMap, err
-		}
-	}
-}
-
-func FindExistingConfigMap(namespace string, service v1alpha1.CacheService, client client.Client) (*corev1.ConfigMap, error) {
-	client = client.WithNamespace(namespace)
-	labels := configMapLabels(service)
-	cmList := &corev1.ConfigMapList{}
-	if err := client.List(labels, cmList); err != nil {
-		return nil, fmt.Errorf("unable to list existing ConfigMaps: %w", err)
-	}
-
-	switch numConfigMaps := len(cmList.Items); numConfigMaps {
-	case 0:
-		return nil, nil
-	case 1:
-		return &cmList.Items[0], nil
-	default:
-		var configMapNames []string
-		for i, cm := range cmList.Items {
-			configMapNames[i] = cm.Name
-		}
-		return nil, fmt.Errorf("expected one ConfigMap, found %d: %s", numConfigMaps, configMapNames)
-	}
-}
-
-// ConfigMapData retrieves all CacheRegion resources related to the passed CacheService, iterates over their Region
-// definitions and adds their json representation to a string:[]byte map that can be used by a ConfigMap
-func ConfigMapData(service v1alpha1.CacheService, k8sClient client.Client) (map[string][]byte, error) {
-	regionList := &v1alpha1.CacheRegionList{}
-	if err := k8sClient.List(service.LabelSelector(), regionList, client.ClusterScoped); err != nil {
-		return nil, fmt.Errorf("unable to list CacheRegion: %w", err)
-	}
-
-	regionMap := make(map[string][]byte, len(regionList.Items))
-	for _, region := range regionList.Items {
-		if bytes, err := marshallRegion(&region); err != nil {
-			return nil, err
-		} else {
-			regionMap[region.Filename()] = bytes
-		}
-	}
-	return regionMap, nil
 }
 
 func marshallRegion(region *v1alpha1.CacheRegion) ([]byte, error) {
