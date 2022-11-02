95140e035b7ace8ccb4ee5f21e1bc3526067bf37
Ryan Emerson <remerson@redhat.com>
2022-11-02 15:48:57 +0000

    Generate EagerCacheRule Webhooks

--- main.go
+++ main.go
@@ -127,6 +127,10 @@ func main() {
 		setupLog.Error(err, "unable to create webhook", "webhook", "LazyCacheRule")
 		os.Exit(1)
 	}
+	if err = (&gingersnapprojectv1alpha1.EagerCacheRule{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "EagerCacheRule")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
