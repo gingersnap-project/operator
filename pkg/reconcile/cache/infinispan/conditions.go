c79725497d1abf28dcc8c2e0e6a3af2b74cd8773
Ryan Emerson <remerson@redhat.com>
2023-02-01 17:22:12 +0000

    Cache CR Ready condition should check cache-manager ServiceBinding status

--- pkg/reconcile/cache/infinispan/conditions.go
+++ pkg/reconcile/cache/infinispan/conditions.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/gingersnap-project/operator/api/v1alpha1"
+	binding "github.com/gingersnap-project/operator/pkg/apis/binding/v1beta1"
 	"github.com/gingersnap-project/operator/pkg/reconcile/cache/context"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
@@ -18,49 +19,72 @@ func ConditionAvailable(c *v1alpha1.Cache, ctx *context.Context) {
 		condition.Message = msg
 	}
 
-	if c.Local() {
-		ds := &appsv1.DaemonSet{}
-		if err := ctx.Client().Load(c.Name, ds); err != nil {
-			ctx.Requeue(fmt.Errorf("unable to load DaemonSet for Available Condition check: %w", err))
-			return
-		}
+	sb := &binding.ServiceBinding{}
+	sbName := c.CacheService().CacheDataServiceBinding()
+	if err := ctx.Client().Load(sbName, sb); err != nil {
+		ctx.Requeue(fmt.Errorf("unable to load ServiceBinding '%s': %w", sbName, err))
+		return
+	}
 
-		if ds.Status.NumberReady == ds.Status.DesiredNumberScheduled {
-			update(
-				metav1.ConditionTrue,
-				"Expected number of DaemonSet pods are Ready",
-			)
-		} else {
-			update(
-				metav1.ConditionFalse,
-				fmt.Sprintf("Required DaemonSet '%d' pods to be Ready, observed '%d'", ds.Status.DesiredNumberScheduled, ds.Status.NumberReady),
-			)
-		}
-	} else {
-		deployment := &appsv1.Deployment{}
-		if err := ctx.Client().Load(c.Name, deployment); err != nil {
-			ctx.Requeue(fmt.Errorf("unable to load Deployment for Available Condition check: %w", err))
-			return
-		}
-
-		var deploymentAvailable bool
-		for _, condition := range deployment.Status.Conditions {
-			if condition.Type == appsv1.DeploymentAvailable {
-				deploymentAvailable = condition.Status == corev1.ConditionTrue
-				break
+	var applicationBound bool
+	for _, condition := range sb.Status.Conditions {
+		if condition.Type == binding.ServiceBindingConditionReady {
+			if condition.Status == metav1.ConditionTrue {
+				applicationBound = true
+			} else {
+				update(
+					metav1.ConditionFalse,
+					fmt.Sprintf("Cache ServiceBinding '%s' not Ready: '%s'", sbName, condition.Message),
+				)
 			}
+			break
 		}
+	}
+	if applicationBound {
+		if c.Local() {
+			ds := &appsv1.DaemonSet{}
+			if err := ctx.Client().Load(c.Name, ds); err != nil {
+				ctx.Requeue(fmt.Errorf("unable to load DaemonSet for Available Condition check: %w", err))
+				return
+			}
 
-		if deploymentAvailable {
-			update(
-				metav1.ConditionTrue,
-				"Expected number of Deployment pods are Ready",
-			)
+			if ds.Status.NumberReady == ds.Status.DesiredNumberScheduled {
+				update(
+					metav1.ConditionTrue,
+					"Expected number of DaemonSet pods are Ready",
+				)
+			} else {
+				update(
+					metav1.ConditionFalse,
+					fmt.Sprintf("Required DaemonSet '%d' pods to be Ready, observed '%d'", ds.Status.DesiredNumberScheduled, ds.Status.NumberReady),
+				)
+			}
 		} else {
-			update(
-				metav1.ConditionFalse,
-				fmt.Sprintf("Required Deployment '%d' pods to be Ready, observed '%d'", deployment.Spec.Replicas, deployment.Status.ReadyReplicas),
-			)
+			deployment := &appsv1.Deployment{}
+			if err := ctx.Client().Load(c.Name, deployment); err != nil {
+				ctx.Requeue(fmt.Errorf("unable to load Deployment for Available Condition check: %w", err))
+				return
+			}
+
+			var deploymentAvailable bool
+			for _, condition := range deployment.Status.Conditions {
+				if condition.Type == appsv1.DeploymentAvailable {
+					deploymentAvailable = condition.Status == corev1.ConditionTrue
+					break
+				}
+			}
+
+			if deploymentAvailable {
+				update(
+					metav1.ConditionTrue,
+					"Expected number of Deployment pods are Ready",
+				)
+			} else {
+				update(
+					metav1.ConditionFalse,
+					fmt.Sprintf("Required Deployment '%d' pods to be Ready, observed '%d'", deployment.Spec.Replicas, deployment.Status.ReadyReplicas),
+				)
+			}
 		}
 	}
 
