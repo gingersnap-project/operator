bd82a4a036b62c7389a5773af9e23b700363e643
Ryan Emerson <remerson@redhat.com>
2023-02-03 12:12:41 +0000

    Ensure that Rule names for a given cache are unique across the cluster

--- api/v1alpha1/eagercacherule_webhook.go
+++ api/v1alpha1/eagercacherule_webhook.go
@@ -1,12 +1,20 @@
 package v1alpha1
 
 import (
+	"context"
+	"errors"
 	"fmt"
+	"net/http"
 
-	"k8s.io/apimachinery/pkg/runtime"
+	admissionv1 "k8s.io/api/admission/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 	ctrl "sigs.k8s.io/controller-runtime"
+	runtimeClient "sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/runtime/inject"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 )
 
 func (r *EagerCacheRule) SetupWebhookWithManager(mgr ctrl.Manager) error {
@@ -36,14 +44,82 @@ func (r *EagerCacheRule) Default() {
 
 //+kubebuilder:webhook:path=/validate-gingersnap-project-io-v1alpha1-eagercacherule,mutating=false,failurePolicy=fail,sideEffects=None,groups=gingersnap-project.io,resources=eagercacherules,verbs=create;update,versions=v1alpha1,name=veagercacherule.kb.io,admissionReviewVersions=v1
 
-var _ webhook.Validator = &EagerCacheRule{}
+// RegisterEagerRuleValidatingWebhook explicitly adds the validating webhook to the Webhook Server
+// This is necessary as we need to implement admission.Handler interface directly so that the request context can be
+// used by the runtime client
+func RegisterEagerRuleValidatingWebhook(mgr ctrl.Manager) {
+	hookServer := mgr.GetWebhookServer()
+	hookServer.Register("/validate-gingersnap-project-io-v1alpha1-eagercacherule", &webhook.Admission{
+		Handler: &eagerRuleValidator{},
+	})
+}
 
-// ValidateCreate implements webhook.Validator so a webhook will be registered for the type
-func (r *EagerCacheRule) ValidateCreate() error {
+type eagerRuleValidator struct {
+	client  runtimeClient.Client
+	decoder *admission.Decoder
+}
+
+var _ inject.Client = &eagerRuleValidator{}
+var _ admission.Handler = &eagerRuleValidator{}
+
+// InjectClient injects the client.
+func (rv *eagerRuleValidator) InjectClient(c runtimeClient.Client) error {
+	rv.client = c
+	return nil
+}
+
+// InjectDecoder injects the decoder.
+func (rv *eagerRuleValidator) InjectDecoder(d *admission.Decoder) error {
+	rv.decoder = d
+	return nil
+}
+
+func (rv *eagerRuleValidator) Handle(ctx context.Context, req admission.Request) admission.Response {
+	rule := &EagerCacheRule{}
+	if req.Operation == admissionv1.Create {
+		err := rv.decoder.Decode(req, rule)
+		if err != nil {
+			return admission.Errored(http.StatusBadRequest, err)
+		}
+
+		err = rv.create(ctx, rule)
+		if err != nil {
+			var apiStatus apierrors.APIStatus
+			if errors.As(err, &apiStatus) {
+				return validationResponseFromStatus(false, apiStatus.Status())
+			}
+			return admission.Denied(err.Error())
+		}
+	}
+
+	if req.Operation == admissionv1.Update {
+		oldrule := &EagerCacheRule{}
+
+		err := rv.decoder.DecodeRaw(req.Object, rule)
+		if err != nil {
+			return admission.Errored(http.StatusBadRequest, err)
+		}
+		err = rv.decoder.DecodeRaw(req.OldObject, oldrule)
+		if err != nil {
+			return admission.Errored(http.StatusBadRequest, err)
+		}
+
+		err = rv.update(rule, oldrule)
+		if err != nil {
+			var apiStatus apierrors.APIStatus
+			if errors.As(err, &apiStatus) {
+				return validationResponseFromStatus(false, apiStatus.Status())
+			}
+			return admission.Denied(err.Error())
+		}
+	}
+	return admission.Allowed("")
+}
+
+func (rv *eagerRuleValidator) create(ctx context.Context, r *EagerCacheRule) error {
 	var allErrs field.ErrorList
 
 	spec := field.NewPath("spec")
-
 	if r.Spec.CacheRef == nil {
 		allErrs = append(allErrs, field.Required(spec.Child("cacheRef"), "'cacheRef' field must be defined"))
 	} else {
@@ -59,19 +135,35 @@ func (r *EagerCacheRule) ValidateCreate() error {
 	} else {
 		RequireNonEmptyArray(&allErrs, "keyColumns", r.Spec.Key.KeyColumns, spec.Child("key"))
 	}
-	return StatusError(allErrs, r.Name, KindEagerCacheRule)
-}
 
-// ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
-func (r *EagerCacheRule) ValidateUpdate(old runtime.Object) error {
-	var allErrs field.ErrorList
-	if err := EnsureRuleImmutability(&allErrs, KindEagerCacheRule, r, old.(*EagerCacheRule)); err != nil {
-		return fmt.Errorf("unable to compare updated rule with existing rule: %w", err)
+	// Ensure that a EagerCacheRule CR with this cacheRef does not already exist in the cluster
+	if len(allErrs) == 0 {
+		cache := &CacheService{
+			Name:      r.Spec.CacheRef.Name,
+			Namespace: r.Spec.CacheRef.Namespace,
+		}
+
+		list := &EagerCacheRuleList{}
+		listOpts := &runtimeClient.ListOptions{
+			LabelSelector: labels.SelectorFromSet(
+				cache.LabelSelector(),
+			),
+		}
+		if err := rv.client.List(ctx, list, listOpts); err != nil {
+			allErrs = append(allErrs, field.InternalError(field.NewPath("spec"), err))
+		} else if len(list.Items) > 0 {
+			rule := &list.Items[0]
+			msg := fmt.Sprintf("EagerCacheRule CR already exists for Cache '%s' with name '%s' in namespace '%s'", cache, rule.Name, rule.Namespace)
+			allErrs = append(allErrs, field.Duplicate(field.NewPath("spec").Child("cacheRef"), msg))
+		}
 	}
 	return StatusError(allErrs, r.Name, KindEagerCacheRule)
 }
 
-// ValidateDelete implements webhook.Validator so a webhook will be registered for the type
-func (r *EagerCacheRule) ValidateDelete() error {
-	return nil
+func (rv *eagerRuleValidator) update(new, old *EagerCacheRule) error {
+	var allErrs field.ErrorList
+	if err := EnsureRuleImmutability(&allErrs, KindEagerCacheRule, new, old); err != nil {
+		return fmt.Errorf("unable to compare updated rule with existing rule: %w", err)
+	}
+	return StatusError(allErrs, new.Name, KindEagerCacheRule)
 }
--- api/v1alpha1/lazycacherule_webhook.go
+++ api/v1alpha1/lazycacherule_webhook.go
@@ -1,12 +1,21 @@
 package v1alpha1
 
 import (
+	"context"
+	"errors"
 	"fmt"
+	"net/http"
 
-	"k8s.io/apimachinery/pkg/runtime"
+	admissionv1 "k8s.io/api/admission/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 	ctrl "sigs.k8s.io/controller-runtime"
+	runtimeClient "sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/runtime/inject"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 )
 
 func (r *LazyCacheRule) SetupWebhookWithManager(mgr ctrl.Manager) error {
@@ -36,10 +45,79 @@ func (r *LazyCacheRule) Default() {
 
 //+kubebuilder:webhook:path=/validate-gingersnap-project-io-v1alpha1-lazycacherule,mutating=false,failurePolicy=fail,sideEffects=None,groups=gingersnap-project.io,resources=lazycacherules,verbs=create;update,versions=v1alpha1,name=vlazycacherule.gingersnap-project.io,admissionReviewVersions=v1
 
-var _ webhook.Validator = &LazyCacheRule{}
+// RegisterLazyRuleValidatingWebhook explicitly adds the validating webhook to the Webhook Server
+// This is necessary as we need to implement admission.Handler interface directly so that the request context can be
+// used by the runtime client
+func RegisterLazyRuleValidatingWebhook(mgr ctrl.Manager) {
+	hookServer := mgr.GetWebhookServer()
+	hookServer.Register("/validate-gingersnap-project-io-v1alpha1-lazycacherule", &webhook.Admission{
+		Handler: &lazyRuleValidator{},
+	})
+}
 
-// ValidateCreate implements webhook.Validator so a webhook will be registered for the type
-func (r *LazyCacheRule) ValidateCreate() error {
+type lazyRuleValidator struct {
+	client  runtimeClient.Client
+	decoder *admission.Decoder
+}
+
+var _ inject.Client = &lazyRuleValidator{}
+var _ admission.Handler = &lazyRuleValidator{}
+
+// InjectClient injects the client.
+func (rv *lazyRuleValidator) InjectClient(c runtimeClient.Client) error {
+	rv.client = c
+	return nil
+}
+
+// InjectDecoder injects the decoder.
+func (rv *lazyRuleValidator) InjectDecoder(d *admission.Decoder) error {
+	rv.decoder = d
+	return nil
+}
+
+func (rv *lazyRuleValidator) Handle(ctx context.Context, req admission.Request) admission.Response {
+	rule := &LazyCacheRule{}
+	if req.Operation == admissionv1.Create {
+		err := rv.decoder.Decode(req, rule)
+		if err != nil {
+			return admission.Errored(http.StatusBadRequest, err)
+		}
+
+		err = rv.create(ctx, rule)
+		if err != nil {
+			var apiStatus apierrors.APIStatus
+			if errors.As(err, &apiStatus) {
+				return validationResponseFromStatus(false, apiStatus.Status())
+			}
+			return admission.Denied(err.Error())
+		}
+	}
+
+	if req.Operation == admissionv1.Update {
+		oldrule := &LazyCacheRule{}
+
+		err := rv.decoder.DecodeRaw(req.Object, rule)
+		if err != nil {
+			return admission.Errored(http.StatusBadRequest, err)
+		}
+		err = rv.decoder.DecodeRaw(req.OldObject, oldrule)
+		if err != nil {
+			return admission.Errored(http.StatusBadRequest, err)
+		}
+
+		err = rv.update(rule, oldrule)
+		if err != nil {
+			var apiStatus apierrors.APIStatus
+			if errors.As(err, &apiStatus) {
+				return validationResponseFromStatus(false, apiStatus.Status())
+			}
+			return admission.Denied(err.Error())
+		}
+	}
+	return admission.Allowed("")
+}
+
+func (rv *lazyRuleValidator) create(ctx context.Context, r *LazyCacheRule) error {
 	var allErrs field.ErrorList
 	if r.Spec.CacheRef == nil {
 		allErrs = append(allErrs, field.Required(field.NewPath("spec").Child("cacheRef"), "'cacheRef' field must be defined"))
@@ -49,18 +127,45 @@ func (r *LazyCacheRule) ValidateCreate() error {
 		RequireField(&allErrs, "namespace", r.Spec.CacheRef.Namespace, cacheRefField)
 	}
 	RequireField(&allErrs, "query", r.Spec.Query, field.NewPath("spec"))
-	return StatusError(allErrs, r.Name, KindLazyCacheRule)
-}
 
-func (r *LazyCacheRule) ValidateUpdate(old runtime.Object) error {
-	var allErrs field.ErrorList
-	if err := EnsureRuleImmutability(&allErrs, KindLazyCacheRule, r, old.(*LazyCacheRule)); err != nil {
-		return fmt.Errorf("unable to compare updated rule with existing rule: %w", err)
+	// Ensure that a LazyCacheRule CR with this cacheRef does not already exist in the cluster
+	if len(allErrs) == 0 {
+		cache := &CacheService{
+			Name:      r.Spec.CacheRef.Name,
+			Namespace: r.Spec.CacheRef.Namespace,
+		}
+
+		list := &LazyCacheRuleList{}
+		listOpts := &runtimeClient.ListOptions{
+			LabelSelector: labels.SelectorFromSet(
+				cache.LabelSelector(),
+			),
+		}
+		if err := rv.client.List(ctx, list, listOpts); err != nil {
+			allErrs = append(allErrs, field.InternalError(field.NewPath("spec"), err))
+		} else if len(list.Items) > 0 {
+			rule := &list.Items[0]
+			msg := fmt.Sprintf("LazyCacheRule CR already exists for Cache '%s' with name '%s' in namespace '%s'", cache, rule.Name, rule.Namespace)
+			allErrs = append(allErrs, field.Duplicate(field.NewPath("spec").Child("cacheRef"), msg))
+		}
 	}
 	return StatusError(allErrs, r.Name, KindLazyCacheRule)
 }
 
-// ValidateDelete implements webhook.Validator so a webhook will be registered for the type
-func (r *LazyCacheRule) ValidateDelete() error {
-	return nil
+func (rv *lazyRuleValidator) update(new, old *LazyCacheRule) error {
+	var allErrs field.ErrorList
+	if err := EnsureRuleImmutability(&allErrs, KindLazyCacheRule, new, old); err != nil {
+		return fmt.Errorf("unable to compare updated rule with existing rule: %w", err)
+	}
+	return StatusError(allErrs, new.Name, KindLazyCacheRule)
+}
+
+// validationResponseFromStatus returns a response for admitting a request with provided Status object.
+func validationResponseFromStatus(allowed bool, status metav1.Status) admission.Response {
+	return admission.Response{
+		AdmissionResponse: admissionv1.AdmissionResponse{
+			Allowed: allowed,
+			Result:  &status,
+		},
+	}
 }
